07a87f6eb0dd141f757af1faf8e38af314b7dffa
Thalys <147563807+garciathalys@users.noreply.github.com>
2025-05-27 18:05:22 -0300

    Update aula9.py

--- aula9.py
+++ aula9.py
@@ -1,8 +1,8 @@
-# nome = input('qual seu nome cpx? ')
+# nome = input('qual seu nome? ')
 # print(f'o seu nome é {nome}')
 
-numero_1 = input('digite um número aí mano: ')
-numero_2 = input('digita outro número ai paizão: ')
+numero_1 = input('digite um número: ')
+numero_2 = input('digita outro número: ')
 int_numero_1 = int(numero_1)
 int_numero_2 = int(numero_2)
 soma = int_numero_1 + int_numero_2
