2bf9a922470be6a05355861c2c0853798ac6c68c
Thalys <147563807+garciathalys@users.noreply.github.com>
2025-05-29 20:53:10 -0300

    Add files via upload

---
+++ aula24.py
@@ -0,0 +1,7 @@
+# for
+texto = 'Python'
+
+novo_texto = ''
+for letra in texto:
+    novo_texto += f'*{letra}'
+print(novo_texto)
---
+++ aula25.py
@@ -0,0 +1,4 @@
+# range -> range(start, stop, step)
+numeros = range(0, 30, 3)
+for numero in numeros:
+    print(numero)
